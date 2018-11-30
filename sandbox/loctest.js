54741b04501efe1448645445a70b430476aee5db
gunther-s <sch14004@byui.edu>
2018-11-30 10:58:31 -0700

    changes

---
+++ sandbox/loctest.js
@@ -0,0 +1,33 @@
+//these functions will work together
+'use strict';
+
+
+
+
+
+// Gets longitude and latitude of current location
+function getGeoLocation() {
+
+} // end getGeoLocation
+
+const STATUS = document.getElementById('status');
+ STATUS.innerHTML = 'Getting Location...';
+
+ // Call the function to get our location
+getGeoLocation();
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+     const LAT = position.coords.latitude;
+     const LONG = position.coords.longitude;
+  
+     // Combine the values
+     const LOCALE = LAT + "," + LONG;
+     console.log(`Lat and Long are: ${LOCALE}.`);
+  
+  
+    })
+   }
+else {
+    STATUS.innerHTML = "Your browser doesn't support Geolocation or it is not enabled!";
+    } // end else
