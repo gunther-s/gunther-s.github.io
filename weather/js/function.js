247d54abd647640ad1d4ba0fc564a932a5642cea
gunther-s <sch14004@byui.edu>
2018-12-01 22:05:11 -0700

    changes

--- weather/js/function.js
+++ weather/js/function.js
@@ -8,39 +8,31 @@ console.log('My javascript is being read.');
 const temp = 31;
 const speed = 5;
 buildWC(speed, temp);
-const direction = "N"; //Set your own value
+const direction = "SW"; //Set your own value
 windDial(direction);
 let bImage ="snowstorm";
 getCondition(bImage);
 let sumImage = getCondition(bImage);
 changeSummaryImage(sumImage);
 
-//Calculate a wind chill temperature
+
 function buildWC(speed, temp) {
     const feelTemp = document.getElementById('feelTemp');
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    console.log(wc)
 
-
-// Compute the windchill
-let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-console.log(wc)
-
-// Round the answer down to integer
 wc = Math.floor(wc);
 
-// If chill is greater than temp, return the temp
 wc = (wc > temp)?temp:wc;
 
-// Display the windchill
 console.log(wc);
 feelTemp.innerHTML = wc;
 }
 
-// Wind Dial Function
+// Make the wind dial
 function windDial(direction){
-    // Get the container
     const dial = document.getElementById("pointer");
     console.log(direction);
-    // Determine the dial class
     switch (direction){
      case "North":
      case "N":
@@ -131,8 +123,8 @@ function getCondition(bImage) {
     return bImage;
 }
 
+
 function changeSummaryImage(sumImage) {
-    let curWeather = document.getElementById("curWeather");
     console.log(sumImage);
 
     switch (sumImage){
