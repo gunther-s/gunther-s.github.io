fe0c71cd1a8d87182f76e625a05fc9cb8f7a50ff
gunther-s <sch14004@byui.edu>
2018-12-08 19:03:40 -0700

    changes

--- weather/js/function.js
+++ weather/js/function.js
@@ -15,7 +15,6 @@
 function buildWC(speed, temp) {
     const feelTemp = document.getElementById('feelTemp');
     
-    // Compute the windchill
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
     console.log(wc);
 
@@ -25,7 +24,6 @@ function buildWC(speed, temp) {
     // If chill is greater than temp, return the temp
     wc = (wc > temp)?temp:wc;
 
-    // Display the windchill
     console.log(wc);
     feelTemp.innerHTML = wc;
 }
